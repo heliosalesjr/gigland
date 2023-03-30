e46a41c04ab01a6f382e56f12bb548eff37fc254
heliosalesjr <heliosalesjr@gmail.com>
2023-03-30 19:47:59 -0300

    Global styles with Emotion

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const StylizedDiv = styled.div`
+    padding: ${props => props.theme.spacing.l};
+    background: ${props => props.theme.colors.secondary.a};
+    border: 2px solid;
+    border-color: ${props => props.theme.colors.primary.a};
+    border-radius: ${props => props.theme.spacing.m};
+`
+
+const Card = ({children}) => {
+  return (
+    <StylizedDiv>
+        {children}
+    </StylizedDiv>
+  )
+}
+
+export default Card
---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ThemeProvider } from '@emotion/react' 
+
+
+const theme = {
+
+    colors: {
+      white: '#FFFFFF',
+      attention: '#FF0E0E',
+      focus: '#B009FF',
+
+      primary: {
+          a: '#5754ED',
+          b: '##D93114',
+          c: '#168070'
+      },
+      secondary: {
+          a: '#EBEAF9',
+          b: '#FDF8F8',
+          c: '#EBFCF9'
+      },
+      neutral: {
+          a: '#373737',
+          b: '#E8E8E8',
+          c: '#F5F5F5'
+      },
+      dark: {
+          a: '#110EA0',
+          b: '#B61B00',
+      }
+    },
+
+    spacing: {
+      xxs: '4px',
+      xs: '8px',
+      s: '16px',
+      m: '24px',
+      l: '32px',
+      xl: '48px',
+      xxl: '64px',
+
+    }
+}
+
+const Theme = ( {children}) => {
+  return (
+    <ThemeProvider theme={theme}> 
+        {children}
+    </ThemeProvider>
+  )
+}
+
+export default Theme
